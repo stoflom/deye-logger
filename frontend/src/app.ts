4e9c5243d0925c342e0926404d7a9b6bbafbdfda
Christoffel Lombard <stoflom@gmail.com>
2026-07-26 15:15:50 +0200

    chore: bump frontend version to 1.3.2 and cleanup testing files

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -5,7 +5,7 @@
 
 /// <reference lib="dom" />
 
-export const FRONTEND_VERSION = "1.3.1";
+export const FRONTEND_VERSION = "1.3.2";
 
 import { ModuleRegistry } from "ag-grid-community";
 import { CsvExportModule, ColumnAutoSizeModule, TextFilterModule, NumberFilterModule, DateFilterModule } from "ag-grid-community";
